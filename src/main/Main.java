976b636eba389749ceb5119fa7fe019a7626378b
leojimenez <ljimenez@sagtech.org>
2025-09-29 21:49:21 -0300

    nuevo estudiante

--- src/main/Main.java
+++ src/main/Main.java
@@ -25,6 +25,9 @@ public class Main {
       
         //Pega tu código aquí abajo
 
+        Estudiante estudianteMaria = new Estudiante("Maria Lopez", "Programación para Testers");
+        estudianteMaria.presentarse();
+
         
 
 }
